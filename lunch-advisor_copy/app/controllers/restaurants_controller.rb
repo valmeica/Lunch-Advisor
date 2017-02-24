7efdd8abb4555e5f7d56b4498ebfc8ce8b2c4202
Valentin Meica <vanjinn@gmail.com>
2017-02-24 05:02:40 +0000

    change

---
+++ lunch-advisor_copy/app/controllers/restaurants_controller.rb
@@ -0,0 +1,59 @@
+class RestaurantsController < ApplicationController
+    before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:new, :edit]
+    
+    def index
+        @restaurants = Restaurant.all.order("created_at DESC")
+        
+        
+    end
+
+    def new
+        @restaurant = current_user.restaurants.build
+    end
+    
+    def show
+        if @restaurant.reviews.blank?
+            @average_review = 0
+        else
+            @average_review = @restaurant.reviews.average(:rating).round(2)
+        end
+    end
+
+    def create
+        @restaurant = current_user.restaurants.build(restaurant_params)
+        
+        if @restaurant.save
+            redirect_to root_path
+        else render 'new'
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @restaurant.update(restaurant_params)
+            redirect_to restaurant_path(@restaurant)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @restaurant.destroy
+        redirect_to root_path
+    end
+    
+    
+    private 
+    
+        def restaurant_params
+            params.require(:restaurant).permit(:name, :description, :lastVisit)
+        end
+        
+        def find_restaurant
+            @restaurant = Restaurant.find(params[:id])
+        end
+    
+end
