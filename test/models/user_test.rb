36c3c8b924c0753f1dcdf1c934140fbf9e363104
Valentin Meica <vanjinn@gmail.com>
2017-02-24 15:20:42 +0000

    adapt to show images on heroku

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+   test 'invalid without name' do
+    user = User.new(email: 'john@example.com')
+    refute user.valid?, 'user is valid without a name'
+    assert_not_nil user.errors[:name], 'no validation error for name present'
+  end
+  
 end
